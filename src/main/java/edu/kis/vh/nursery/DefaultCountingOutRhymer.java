c968dcd985ed03a77f6f309bbf5afe5265fab0df
niuest4127 <251125@edu.p.lodz.pl>
2025-03-27 17:08:32 +0100

    zastapienie literalow oraz dodanie final i @Override

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,10 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-
-    private int[] NUMBERS = new int[12];
-
-    public int total = -1;
+    int final size = 12;
+    private int[] NUMBERS = new int[size];
+    int final err_code = -1;
+    public int total = err_code;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,7 +12,7 @@ public class DefaultCountingOutRhymer {
     }
 
         public boolean callCheck() {
-            return total == -1;
+            return total == err_code;
         }
         
             public boolean isFull() {
@@ -21,13 +21,13 @@ public class DefaultCountingOutRhymer {
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return -1;
+                        return err_code;
                     return NUMBERS[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return -1;
+                            return err_code;
                         return NUMBERS[total--];
                     }
 
