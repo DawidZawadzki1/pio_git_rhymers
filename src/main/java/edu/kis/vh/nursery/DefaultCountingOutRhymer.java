a9ee3863e6d6a165e7f8c641d7fa468a02ab7558
DawidZawadzki <248352@edu.p.lodz.pl>
2025-04-03 16:34:15 +0200

    Dzial 2, accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,34 +1,38 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-    int final SIZE = 12;
+    private static final int SIZE = 12;
     private int[] numbers = new int[SIZE];
-    int final ERR_CODE = -1;
-    public int total = ERR_CODE;
+    private static final int  ERR_CODE = -1;
+    private int total = ERR_CODE;
+
+    public int getTotal(){
+        return total;
+    }
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-        public boolean callCheck() {
-            return total == ERR_CODE;
-        }
-        
-            public boolean isFull() {
-                return total == 11;
-            }
-        
-                protected int peekaboo() {
-                    if (callCheck())
-                        return ERR_CODE;
-                    return numbers[total];
-                }
-            
-                    public int countOut() {
-                        if (callCheck())
-                            return ERR_CODE;
-                        return numbers[total--];
-                    }
+    public boolean callCheck() {
+        return total == ERR_CODE;
+    }
 
+    public boolean isFull() {
+
+        return total == SIZE-1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return ERR_CODE;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return ERR_CODE;
+        return numbers[total--];
+    }
 }
--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
     
     @Override
     public int countOut() {
         while (!callCheck())
-            
+
         temp.countIn(super.countOut());
         
         int ret = temp.countOut();
